3299a573a3e21bf11f8a7eeb21fd1823aee9ceba
LucioFurnari <luciofurnari99@gmail.com>
2023-04-11 15:42:05 -0300

    Add Nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-
+import { Link } from "react-router-dom"
+import Nav from "./components/Nav"
 function App() {
 
   return (
     <div className="App">
+      <Nav />
+      <h1>Welcome to Home</h1>
+      <Link to='/shop'>To Shop</Link>
     </div>
   )
 }
