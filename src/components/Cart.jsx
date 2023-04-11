74d60b4fa344b75b5c81d399748c9a6402607cc5
LucioFurnari <luciofurnari99@gmail.com>
2023-04-11 16:23:29 -0300

    Add Nav component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,9 @@
+import Nav from "./Nav";
+
 export default function Cart() {
   return(
     <div>
+      <Nav />
       <h1>Welcome to the cart</h1>
     </div>
   )
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom"
+import Nav from "./Nav"
 export default function Shop() {
   return (
     <div>
+      <Nav />
       <h1>Welcome to shop</h1>
       <Link to='/'>To Home</Link>
     </div>
