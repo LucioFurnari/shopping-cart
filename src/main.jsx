172cd04024cfce90e5b3adbcdc842fada7b4ec1b
LucioFurnari <luciofurnari99@gmail.com>
2023-04-11 15:42:38 -0300

    Use react router, to create the client side routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Shop from './components/Shop'
+import Cart from './components/Cart'
+import {Routes, Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import './index.css'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <>
+    <Route path='/' element={<App />}/>
+    <Route path='/shop' element={<Shop />}/>
+    <Route path='/cart' element={<Cart/>}/>
+  </>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
