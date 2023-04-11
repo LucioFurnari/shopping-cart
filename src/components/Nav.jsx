5eeb55c44e7d209c4da53b39522a41433d3be443
LucioFurnari <luciofurnari99@gmail.com>
2023-04-11 15:41:09 -0300

    Create nav components with Link

---
+++ src/components/Nav.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom"
+export default function Nav() {
+  return(
+    <nav>
+      <Link to='/'>Home</Link>
+      <Link to='/shop'>Shop</Link>
+      <Link to='/cart'>Cart</Link>
+    </nav>
+  )
+}
