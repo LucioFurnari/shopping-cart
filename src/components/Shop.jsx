c84b981c1a679d67d13ad38c4b98b30e466de6ba
LucioFurnari <luciofurnari99@gmail.com>
2023-04-11 15:41:36 -0300

    Create Shop component

---
+++ src/components/Shop.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom"
+export default function Shop() {
+  return (
+    <div>
+      <h1>Welcome to shop</h1>
+      <Link to='/'>To Home</Link>
+    </div>
+  )
+}
