f01d8f5e9871238c9ee2f8a47ae73418321db08d
LucioFurnari <luciofurnari99@gmail.com>
2023-04-11 23:17:58 -0300

    Use the fake db, to crea a simple list of cards

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom"
+import dataBase from "./db"
 import Nav from "./Nav"
+import Card from "./Card"
 export default function Shop() {
   return (
     <div>
       <Nav />
-      <h1>Welcome to shop</h1>
+      <h1>Welcome to Shop</h1>
       <Link to='/'>To Home</Link>
+      {dataBase.map((item,index) => {
+        const { n } = item;
+        return <Card {...item} key={n}/>
+      })}
     </div>
   )
 }
