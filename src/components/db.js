c1ff3a01032046b405d24c89785ce67561b8d8f3
LucioFurnari <luciofurnari99@gmail.com>
2023-04-11 22:34:07 -0300

    Create simple fake data base to the shop

---
+++ src/components/db.js
@@ -0,0 +1,14 @@
+const data = [
+  {
+    name: 'Chocolate Bar',
+    price: 100,
+  },
+  {
+    name: 'Chocolate with peanuts',
+    price: 120,
+  },
+  {
+    name: 'Mint chocolate',
+    price: 175,
+  }
+]
