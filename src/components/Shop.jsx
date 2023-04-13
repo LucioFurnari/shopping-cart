243ce1ef3486da2113fc68b24a7573358e140d12
LucioFurnari <luciofurnari99@gmail.com>
2023-04-12 22:27:54 -0300

    Pass new props to the Card, and add class name to container

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom"
 import dataBase from "./db"
-import Nav from "./Nav"
 import Card from "./Card"
-export default function Shop() {
+
+export default function Shop(props) {
+  const {func} = props
   return (
     <div>
-      <Nav />
       <h1>Welcome to Shop</h1>
       <Link to='/'>To Home</Link>
-      {dataBase.map((item,index) => {
-        const { n } = item;
-        return <Card {...item} key={n}/>
-      })}
+      <div className="grid-container">
+        {dataBase.map((item,index) => {
+          const { n } = item;
+          return <Card {...item} key={n} id={index} handleClick={func}/>
+          })
+        }
+      </div>
     </div>
   )
 }
