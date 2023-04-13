b0df65ce29ccba6aa8d43f6e56fea6e09d3bfb60
LucioFurnari <luciofurnari99@gmail.com>
2023-04-12 22:24:19 -0300

    Create all routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import { Link } from "react-router-dom"
-import Nav from "./components/Nav"
+import { Routes, Route } from "react-router-dom";
+import {Root, loader as RootLoader} from './routes/root';
+import ErrorPage from './error.page';
+import Cart from './components/Cart';
+import Shop from './components/Shop'
+import dataBase from './components/db';
+
 function App() {
+  const cart = []
+  function buyProduct(ev) {
+    const {id} = ev.target;
+    dataBase.map((item, index) => {
+    if(index == id) {
+      cart.push(item)
+    }
+  })
+  console.log(cart)
+}
+
 
   return (
     <div className="App">
-      <Nav />
-      <h1>Welcome to Home</h1>
-      <Link to='/shop'>To Shop</Link>
+    <Routes>
+      <Route path='/' element={<Root />} errorElement={<ErrorPage />}>
+        <Route path='/shop' element={<Shop func={buyProduct}/>} />
+        <Route path='/cart' element={<Cart list={cart}/>}/>
+      </Route>
+    </Routes>
     </div>
   )
 }
