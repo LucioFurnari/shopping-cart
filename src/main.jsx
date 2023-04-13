50ef0208936e614d4342cc1e2ad5e11b2d2092ba
LucioFurnari <luciofurnari99@gmail.com>
2023-04-12 22:23:51 -0300

    Pass all routes to App

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import Shop from './components/Shop'
-import Cart from './components/Cart'
-import {Routes, Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import {BrowserRouter } from 'react-router-dom'
 import './index.css'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-  <>
-    <Route path='/' element={<App />}/>
-    <Route path='/shop' element={<Shop />}/>
-    <Route path='/cart' element={<Cart/>}/>
-  </>
-  )
-)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
