1ecdba10f7d90aa5a62ec7c90ee7c185bf1ae52b
LucioFurnari <luciofurnari99@gmail.com>
2023-04-15 14:42:02 -0300

    Fix the function to add purchase to the cart, to detect if it have already a similar purchase

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,42 @@ import {Root, loader as RootLoader} from './routes/root';
 import ErrorPage from './error.page';
 import Cart from './components/Cart';
 import Shop from './components/Shop'
-import dataBase from './components/db';
-import { useState } from "react";
+import Nav from './components/Nav'
+import shop from './components/db';
+import { useEffect, useState } from "react";
+import { createElement } from "react";
 
 function App() {
   const [cart,setCart] = useState([]);
   function buyProduct(ev) {
     const {id} = ev.target;
-    const newArr = dataBase.filter((item, index) => {
-    if(index == id) {
-      return item
-    }
+    const newItem = shop.filter((item,index) => {
+      if(index == id) {
+        return item
+      }
     })
-    setCart([...cart, ...newArr])
-    console.log(cart)
-}
-
+    const list = cart.filter((item) => {if(item.name == newItem[0].name)return item})
+    if (list.length != 0) {
+      let arr = cart.map(item => {
+        if(item.name == newItem[0].name) {
+          return {...item, price: item.price + newItem[0].price}
+        } else {
+          return item
+        }
+      })
+      setCart(arr)
+    } else {
+      setCart([...cart, ...newItem])
+    }
+  }
 
   return (
     <div className="App">
+    <Nav />
     <Routes>
-      <Route path='/' element={<Root />} errorElement={<ErrorPage />}>
-        <Route path='/shop' element={<Shop func={buyProduct}/>} />
-        <Route path='/cart' element={<Cart list={cart}/>}/>
-      </Route>
+      <Route path='/' element={<Root />} errorElement={<ErrorPage />}/>
+      <Route path='/shop' element={<Shop func={buyProduct}/>} />
+      <Route path='/cart' element={<Cart list={cart}/>}/>
     </Routes>
     </div>
   )
