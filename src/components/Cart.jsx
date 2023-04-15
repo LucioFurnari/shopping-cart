0b1922a04f82457ce78dca7d5a45d3b1845d4679
LucioFurnari <luciofurnari99@gmail.com>
2023-04-15 14:42:58 -0300

    Add simple card container to the purchases

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -6,7 +6,12 @@ export default function Cart(props) {
       <h1>Welcome to the cart</h1>
       <div className="grid-container">
         {list.map((item, index) => {
-          return <div className="card" key={index}><p>{item.name}</p></div>
+          return (
+          <div className="card" key={index}>
+            <p>{item.name}</p>
+            <p>{item.price}</p>
+          </div>
+          )
         })}
       </div>
     </div>
