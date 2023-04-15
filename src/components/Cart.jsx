4202b513551cfc61d3f56ed480c1d7f242966bfe
LucioFurnari <luciofurnari99@gmail.com>
2023-04-15 17:03:40 -0300

    Add total price prop

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,18 +1,22 @@
-
 export default function Cart(props) {
-  const { list } = props
+  const { list, total } = props
   return(
     <div>
       <h1>Welcome to the cart</h1>
       <div className="grid-container">
         {list.map((item, index) => {
+        const {name, price, quantity} = item;
           return (
           <div className="card" key={index}>
-            <p>{item.name}</p>
-            <p>{item.price}</p>
+            <p>{name}</p>
+            <p>{price} $</p>
+            <p>{quantity > 1 ? 'x'+quantity : null}</p>
           </div>
           )
         })}
+      </div>
+      <div>
+        <p>Total price: {total}</p>
       </div>
     </div>
   )
