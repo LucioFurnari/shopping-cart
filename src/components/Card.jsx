0158a04032645f59cd5755a8e40833857fd71369
LucioFurnari <luciofurnari99@gmail.com>
2023-04-16 16:25:23 -0300

    Add increase/decrease quantity buttons, state and functions

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Card(props) {
   const { name, price, id , handleClick} = props;
+  const [quantity, setQuantity] = useState(1);
+  function increaseQuantity() {
+    setQuantity(quantity + 1)
+  }
+  function decreaseQuantity() {
+    setQuantity(quantity > 1 ? quantity - 1 : quantity)
+  }
   return (
     <div className="card">
       <h2>{name}</h2>
-      <p>{price}</p>
-      <button id={id} onClick={handleClick}>Buy</button>
+      <p>{price} $</p>
+      <button id={id} onClick={(ev) => handleClick(ev, quantity)}>Buy</button>
+      <div className="quantity-container">
+        <button onClick={decreaseQuantity}>-</button>
+        <span>{quantity}</span>
+        <button onClick={increaseQuantity}>+</button>
+      </div>
     </div>
   )
 }
