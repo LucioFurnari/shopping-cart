06de80efd9e4010a8fe9df2bf0a4f1231f660eeb
LucioFurnari <luciofurnari99@gmail.com>
2023-04-16 17:56:06 -0300

    Add images to the products

--- src/components/db.js
+++ src/components/db.js
@@ -2,15 +2,18 @@ const dataBase = [
   {
     name: 'Chocolate Bar',
     price: 100,
+    img: '../src/assets/images/chocolatebarra.jpg',
     n: 1,
   },
   {
     name: 'Chocolate with peanuts',
     price: 120,
+    img: '../src/assets/images/chocolatemani.jpg',
     n: 2,
   },
   {
     name: 'Mint chocolate',
+    img: '../src/assets/images/chocolatementa.jpg',
     price: 175,
     n: 3,
   }
