19d6d1ab30bde82f54271369005de50eb08ec239
LucioFurnari <luciofurnari99@gmail.com>
2023-04-15 23:04:51 -0300

    Minor changes

--- src/components/db.js
+++ src/components/db.js
@@ -1,14 +1,19 @@
-const data = [
+const dataBase = [
   {
     name: 'Chocolate Bar',
     price: 100,
+    n: 1,
   },
   {
     name: 'Chocolate with peanuts',
     price: 120,
+    n: 2,
   },
   {
     name: 'Mint chocolate',
     price: 175,
+    n: 3,
   }
 ]
+
+export default dataBase;
