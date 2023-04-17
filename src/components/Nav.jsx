62c7f5aa4f97a017fc9a44370f80f59618446af7
LucioFurnari <luciofurnari99@gmail.com>
2023-04-16 21:03:25 -0300

    Add total quantity in the nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom"
-export default function Nav() {
+export default function Nav(props) {
+  const {total} = props;
   return(
     <nav>
       <Link to='/'>Home</Link>
       <Link to='/shop'>Shop</Link>
-      <Link to='/cart'>Cart</Link>
+      <Link to='/cart'>Cart{total > 0 ? total : null}</Link>
     </nav>
   )
 }
