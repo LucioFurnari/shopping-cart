dcc719a7b321741607e91ae89a311238af0e66d9
LucioFurnari <luciofurnari99@gmail.com>
2023-04-17 11:57:21 -0300

    Change the url of the image with import

--- src/components/db.js
+++ src/components/db.js
@@ -1,19 +1,22 @@
+import chocolateBar from '../assets/images/chocolatebarra.jpg'
+import chocolateMani from '../assets/images/chocolatemani.jpg'
+import chocolateMenta from '../assets/images/chocolatementa.jpg'
 const dataBase = [
   {
     name: 'Chocolate Bar',
     price: 100,
-    img: '../src/assets/images/chocolatebarra.jpg',
+    img: chocolateBar,
     n: 1,
   },
   {
     name: 'Chocolate with peanuts',
     price: 120,
-    img: '../src/assets/images/chocolatemani.jpg',
+    img: chocolateMani,
     n: 2,
   },
   {
     name: 'Mint chocolate',
-    img: '../src/assets/images/chocolatementa.jpg',
+    img: chocolateMenta,
     price: 175,
     n: 3,
   }
