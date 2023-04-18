22c404258305eced78f922a572c757ac2bf3e697
LucioFurnari <luciofurnari99@gmail.com>
2023-04-18 19:53:15 -0300

    Add span to the cart icon to show the number of products

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom"
+import { AiOutlineShoppingCart  } from 'react-icons/ai'
 export default function Nav(props) {
   const {total} = props;
   return(
     <nav>
       <h1>CHOCOLAT</h1>
-      <div>
+      <div className="nav-link-container">
       <Link to='/'>Home</Link>
       <Link to='/shop'>Shop</Link>
-      <Link to='/cart' >Cart {total > 0 ? total : null}</Link>
+      <Link to='/cart' className="cart-link">
+        <AiOutlineShoppingCart /> {total > 0 ? <span>{total}</span> : null}
+      </Link>
       </div>
     </nav>
   )
