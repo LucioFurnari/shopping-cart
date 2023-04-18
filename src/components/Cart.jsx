1e9db3110e9bd766cf1d5e22fa1664beb07f99a1
LucioFurnari <luciofurnari99@gmail.com>
2023-04-18 00:17:46 -0300

    Add simple empty cart message

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,22 +1,32 @@
+import emptyCart from '../assets/images/empty-cart.svg'
+
 export default function Cart(props) {
   const { list, total, handleDelete, handlePurchase } = props
   return(
     <div>
       <h1>Welcome to the cart</h1>
+      {(list.length > 0)
+      ? 
       <div className="grid-container">
-        {list.map((item, index) => {
+      {list.map((item, index) => {
         const {name, price, quantity, img} = item;
-          return (
-          <div className="card" key={index}>
-            <p>{name}</p>
-            <p>{price} $</p>
-            <img src={img}></img>
-            <p>{quantity > 1 ? 'x' + quantity : null}</p>
-            <button id={index} onClick={(ev) => handleDelete(ev, quantity)}>X</button>
-          </div>
-          )
-        })}
+        return (
+        <div className="card" key={index}>
+          <p>{name}</p>
+          <p>{price} $</p>
+          <img src={img}></img>
+          <p>{quantity > 1 ? 'x' + quantity : null}</p>
+          <button id={index} onClick={(ev) => handleDelete(ev, quantity)}>X</button>
+        </div>
+        )
+      })}
       </div>
+      :
+      <div className="empty-cart">
+        <img src={emptyCart}></img>
+        <p>The cart is empty</p>
+      </div>
+      }
       <div>
         <p>Total price: {total}</p>
         <button onClick={handlePurchase}>Purchase</button>
