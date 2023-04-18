a103838da44425f21ef62a933e1c9ac528e6a039
LucioFurnari <luciofurnari99@gmail.com>
2023-04-18 16:47:34 -0300

    Add title to the nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,9 +3,12 @@ export default function Nav(props) {
   const {total} = props;
   return(
     <nav>
+      <h1>CHOCOLAT</h1>
+      <div>
       <Link to='/'>Home</Link>
       <Link to='/shop'>Shop</Link>
       <Link to='/cart' >Cart {total > 0 ? total : null}</Link>
+      </div>
     </nav>
   )
 }
