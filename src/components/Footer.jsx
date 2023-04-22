462cd24fbe0577720b99c92c7f737f44589b3fa2
LucioFurnari <luciofurnari99@gmail.com>
2023-04-21 23:25:50 -0300

    Add icons for the media links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
+import { AiOutlineInstagram, AiOutlineFacebook } from 'react-icons/ai'
+import { CiTwitter } from 'react-icons/ci'
 export default function Footer() {
   return(
     <footer>
-      <p>Footer placeholder</p>
+      <p>© 2023 Chocolat</p>
+      <ul>
+        <li><a><AiOutlineInstagram /></a></li>
+        <li><a><CiTwitter /></a></li>
+        <li><a><AiOutlineFacebook /></a></li>
+      </ul>
     </footer>
   )
 }
