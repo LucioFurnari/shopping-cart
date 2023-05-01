eafdebecaa594af699e48f0a6e1be0c575603dee
LucioFurnari <luciofurnari99@gmail.com>
2023-04-30 22:29:44 -0300

    Add more style

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,6 @@
 import dataBase from "../components/db"
+import HeroSection from "../components/Hero";
+import Gallery from "../components/Gallery";
 import { Link } from "react-router-dom"
 
 export async function loader() {
@@ -7,10 +9,14 @@ export async function loader() {
 
 export function Root() {
   return(
-    <div className="home">
-      <h2>Taste good, does good</h2>
-      <Link to='/shop'>To the Shop</Link>
+    <>
+    <div className="home flex flex-col justify-center items-center bg-hero-pattern bg-center bg-cover bg-no-repeat h-screen">
+      <h2 className="text-4xl md:text-6xl xl:text-8xl text-orange-100">Taste good, does good</h2>
+      <Link className="text-4xl  text-orange-100" to='/shop'>To the Shop</Link>
     </div>
+      <HeroSection />
+      <Gallery />
+    </>
   )
 }
 
