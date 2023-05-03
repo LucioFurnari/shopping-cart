0c009b2e5388ef37a275987a9e0ac871df41cec3
LucioFurnari <luciofurnari99@gmail.com>
2023-05-03 19:17:13 -0300

    Change the style of the cards, comment the quantity buttons

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Card(props) {
   const { name, price, id , handleClick, img} = props;
@@ -10,14 +11,14 @@ export default function Card(props) {
     setQuantity(quantity > 1 ? quantity - 1 : quantity)
   }
   return (
-    <div className="card">
-      <img src={img} ></img>
-      <p>{name}</p>
-      <p>{price} $</p>
+    <div className="card flex flex-col justify-left items-star bg-zinc-300">
+      <img className=" h-full" src={img} ></img>
+      <p className="text-2xl p-2">{name}</p>
+      <p className="text-xl p-2">{price}.00 $</p>
       <div className="quantity-container">
-        <button onClick={decreaseQuantity}>-</button>
+        {/* <button onClick={decreaseQuantity}>-</button>
         <span>{quantity}</span>
-        <button onClick={increaseQuantity}>+</button>
+        <button onClick={increaseQuantity}>+</button> */}
       </div>
       <button id={id} onClick={(ev) => {
         handleClick(ev, quantity)
@@ -25,6 +26,7 @@ export default function Card(props) {
         }} className="buy-btn">
         Buy
       </button>
+      <Link className="border-solid border-2 hover:border-sky-700" to={name}>Item</Link>
     </div>
   )
 }
