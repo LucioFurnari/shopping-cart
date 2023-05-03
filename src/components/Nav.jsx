bdfad2e848b4d7fbc2642debe027752894d3bcb7
LucioFurnari <luciofurnari99@gmail.com>
2023-05-03 15:37:14 -0300

    Add function to change the nav position depending of the scroll

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,29 @@
 import { Link } from "react-router-dom"
+import { useEffect, useState } from "react";
 import { AiOutlineShoppingCart  } from 'react-icons/ai'
 export default function Nav(props) {
   const {total} = props;
+  const [scroll, setScroll] = useState(false);
+
+  const handleScroll = (ev) => {
+    setScroll(window.pageYOffset > 200 ? true : false)
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  return () => {
+    window.removeEventListener("scroll", handleScroll);
+  };
+  },[]);
+
   return(
-    <nav className="flex justify-around items-center bg-yellow-900">
+    <nav className= {`${scroll ? "bg-yellow-900 fixed w-full" : "bg-transparent w-full absolute " }  transition-all duration-300 will-change-auto flex justify-around items-center `} onScroll={handleScroll}>
       <h1 className="text-orange-200 text-lg">CHOCOLAT</h1>
       <ul className="nav-link-container flex items-center">
-        <li className="ml-4 text-zinc-100"><Link className="transition ease-in-out 150ms bg  p-4 inline-block hover:bg-orange-100  hover:text-gray-800 text-lg"  to='/'>Home</Link></li>
-        <li className="ml-4 text-zinc-100"><Link className="transition ease-in-out 150ms bg p-4 inline-block hover:bg-orange-100  hover:text-gray-800 text-lg" to='/shop'>Shop</Link></li>
+        <li className="ml-4 text-zinc-100"><Link className="transition ease-in-out 150ms bg  p-4 inline-block hover:bg-orange-100  hover:text-gray-800 text-lg rounded-md"  to='/'>Home</Link></li>
+        <li className="ml-4 text-zinc-100"><Link className="transition ease-in-out 150ms bg p-4 inline-block hover:bg-orange-100  hover:text-gray-800 text-lg  rounded-md"  to='/shop'>Shop</Link></li>
         <li className="ml-4 text-zinc-100">
-          <Link className="group transition ease-in-out 150ms bg p-6 inline-block hover:bg-orange-100 cart-link text-lg" to='/cart'>
+          <Link className="group transition ease-in-out 150ms bg p-5 inline-block hover:bg-orange-100 cart-link text-lg" to='/cart'>
             <AiOutlineShoppingCart className="group-hover:fill-gray-800"/> {total > 0 ? <span>{total}</span> : null}
           </Link>
         </li>
