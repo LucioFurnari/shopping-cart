15832a1f6e08c71f14c2eac0dbe0e668b1c979de
LucioFurnari <luciofurnari99@gmail.com>
2023-05-04 18:08:06 -0300

    Add more data to the data base

--- src/components/db.js
+++ src/components/db.js
@@ -6,33 +6,48 @@ import espresso from '../assets/images/espresso.jpg'
 const dataBase = [
   {
     name: 'Chocolate Bar',
+    description: "Here's a chocolate bar that will make your taste buds go wild. Made of dark chocolate and coated in premium cocoa butter, this bar is sure to give you a sweet experience that will leave you wanting more. Whether you're looking for a quick snack or something to satisfy your cravings all day long, this chocolate bar is perfect for you.",
+    type: 'Chocolate',
     price: 100,
     img: chocolateBar,
     n: 1,
+    stock: 25,
   },
   {
     name: 'Chocolate with peanuts',
+    description: '',
+    type: 'Chocolate',
     price: 120,
     img: chocolateMani,
     n: 2,
+    stock: 21,
   },
   {
     name: 'Mint chocolate',
+    description: '',
+    type: 'Chocolate',
     img: chocolateMenta,
     price: 175,
     n: 3,
+    stock: 30,
   },
   {
     name: 'Iced coffee',
+    description: '',
+    type: 'Coffee',
     img: icedCoffe,
     price: 120,
     n: 4,
+    stock: 40,
   },
   {
     name: 'Espresso',
+    type: 'Coffee',
+    description: '',
     img: espresso,
     price: 120,
     n: 5,
+    stock: 45,
   }
 ]
 
