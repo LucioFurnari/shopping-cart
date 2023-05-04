6d82f4900b23fa1c2f36280408e01a2d78311401
LucioFurnari <luciofurnari99@gmail.com>
2023-05-04 17:48:46 -0300

    Change the margin of the a tag to the list tag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ export default function Footer() {
     <footer className='flex items-center justify-around py-4 bg-yellow-900'>
       <p className='text-zinc-100'>© 2023 Chocolat</p>
       <ul className='flex '>
-        <li><a className=' ml-6'><AiOutlineInstagram className=' fill-zinc-100'/></a></li>
-        <li><a className=' ml-6'><CiTwitter className='fill-zinc-100'/></a></li>
-        <li><a className=' ml-6'><AiOutlineFacebook className='fill-zinc-100'/></a></li>
+        <li className=' ml-6'><a><AiOutlineInstagram className=' fill-zinc-100'/></a></li>
+        <li className=' ml-6'><a><CiTwitter className='fill-zinc-100'/></a></li>
+        <li className=' ml-6'><a><AiOutlineFacebook className='fill-zinc-100'/></a></li>
       </ul>
     </footer>
   )
