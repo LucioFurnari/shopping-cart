75f98f2a9158ee0cbe0d381bc323ab54843f0825
LucioFurnari <luciofurnari99@gmail.com>
2023-05-08 16:40:24 -0300

    Change the added action to detect if a item was selected

--- src/components/ShopContext.js
+++ src/components/ShopContext.js
@@ -7,8 +7,22 @@ export const WishlistDispatchContext = createContext(null);
 export function wishlistReducer(list, action) {
   switch (action.type) {
     case 'added': {
-      const [newItem] = dataBase.filter((item,index) => item.n == action.id)
-      return [...list, newItem]
+      let [newItem] = dataBase.filter((item,index) => item.n == action.id)
+      // Detect if the list is empty or have the same item selected //
+      const copyArr = list.filter((item) => (item.n == newItem.n))
+      if (copyArr.length != 0) {
+        const newList = list.map((item,index) => {
+        if (item.n == newItem.n) {
+          return {...item, newItem}
+        } else {
+          return item
+        }
+        })
+        return newList
+      } else {
+        const newArr = [...list, newItem]
+        return newArr
+      }
     }
     case 'remove': {
       return list.filter((item,index) => item.n != action.id)
