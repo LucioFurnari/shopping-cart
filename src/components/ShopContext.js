b78b274e6826cbe80ca34f8f6ed4cac6da68aaf5
LucioFurnari <luciofurnari99@gmail.com>
2023-05-08 20:47:25 -0300

    Create new context and dispatch function for the cart section

--- src/components/ShopContext.js
+++ src/components/ShopContext.js
@@ -1,9 +1,15 @@
 import { createContext } from "react";
 import dataBase from "./db";
 
+// Wishlist context //
 export const WishListContext = createContext(null);
 export const WishlistDispatchContext = createContext(null);
 
+// Cart/Shop context //
+export const cartContext = createContext(null);
+export const cartDispatchContext = createContext(null);
+
+// Wishlist functions //
 export function wishlistReducer(list, action) {
   switch (action.type) {
     case 'added': {
@@ -30,5 +36,39 @@ export function wishlistReducer(list, action) {
     default: {
       throw Error('Unknown action: ' + action.type);
     }
+  }
+}
+
+// Shop section functions //
+
+export function shopSectionReducer(item, action) {
+  const {cart} = item;
+  switch (action.type) {
+    case 'ADD-TO-CART': {
+      const {id} = ev.target;
+      const [newItem] = dataBase.filter((item,index) => {
+      if(item.n == id) {
+        return item
+      }
+      })
+      const list = cart.filter((item) => item.name == newItem.name)
+      if (list.length != 0) {
+      const newArr = cart.map(item => {
+      if(item.name == newItem.name) {
+        return {...item, newItem}
+      } else {
+        return item
+      }
+      })
+      return newArr;
+      } else {
+      setCart([...cart, {
+        name: newItem.name,
+        price: (newItem.price*quantity),
+        img: newItem.img,
+        quantity,
+        }])
+      }
+      }
   }
 }
