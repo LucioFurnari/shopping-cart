0dc9eec01f8ba4baa232ca96703c88fb16eddc95
LucioFurnari <luciofurnari99@gmail.com>
2023-05-08 16:43:26 -0300

    Add comment for help, Header component

--- src/components/product-page/Product.jsx
+++ src/components/product-page/Product.jsx
@@ -1,34 +1,32 @@
 import ItemCard from "./ItemCard";
+import Header from "../ui/Header";
 import dataBase from "../db";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-export default function Item() {
-  const [favorite, setFavorite] = useState(false);
+export default function Product({func}) {
   const [seeDesc, setSeeDesc] = useState(false);
-  const [product, setProduct] = useState([])
-  const { item } =  useParams();
-  useEffect(() => setProduct([dataBase.find(elem => elem.name == item)]),[])
-  const handleFavorite = () => setFavorite(favorite => !favorite);
+  const [product, setProduct] = useState([]);
   const handleDescription = () => setSeeDesc(seeDesc => !seeDesc);
+
+  // Use the param from the route to call the item from the dataBase //
+  const { item } =  useParams();
+  useEffect(() => setProduct([dataBase.find(elem => elem.name == item)]),[]);
 
   return(
     <section >
-      <div className="flex flex-col  items-center justify-center text-orange-100 bg-zinc-900 pt-20 pb-20">
-        <h3 className="text-4xl md:text-6xl mb-6">Product</h3>
-        <span className="text-md md:text-xl"><Link>All</Link> / {item}</span>
-      </div>
+      <Header section='Product' link={'/shop'} routeName={'All'} item={item}/>
       <div className="grid-container p-12 pt-20 justify-center grid grid-cols-1 grid-rows-1">
-      {product.map((item,index) => {
+      {
+        product.map((item,index) => {
         const { img, n, name, price, type, stock, description } = item;
         return (
           <ItemCard 
           props={item} 
           key={index}
-          handleFavorite={handleFavorite} 
           handleDescription={handleDescription}
-          favorite={favorite}
           seeDesc={seeDesc}
+          handlePurchase={func}
           />
         )})
       }
