e4c9ce7dee7da7d54b781f4ce147f3cdd00a9e16
LucioFurnari <luciofurnari99@gmail.com>
2023-05-08 00:50:35 -0300

    Add fix the pass of the prop to the children component

--- src/components/wishlist-page/Wishlist.jsx
+++ src/components/wishlist-page/Wishlist.jsx
@@ -1,17 +1,19 @@
 import { useContext } from "react"
 import Header from "../ui/Header"
 import WishlistCard from "./WishlistCard"
-import { WishlistContext } from "../../App"
+import { WishListContext } from "../ShopContext"
 
 export default function Wishlist () {
-  const wishlist = useContext(WishlistContext)
-  console.log(wishlist)
+  const wishlist = useContext(WishListContext)
+
   return(
     <section className=" h-screen">
       <Header section={'Wishlist'} link={'/'} routeName={'Home'} item={'wishlist'} />
-      { <div className="grid grid-cols-1"> 
-        {wishlist.map((itemObj,index) => <WishlistCard props={itemObj} key={index}/>)}
-      </div> }
+      <div className="grid grid-cols-1"> 
+        {wishlist.map((itemObj,index) =>{ 
+        return( <WishlistCard {...itemObj} key={index}/>)
+        })}
+      </div>
     </section>
   )
 }
