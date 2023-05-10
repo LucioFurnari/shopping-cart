7b5df761c36fd1ee54ceb4a5a44255c79b42e553
LucioFurnari <luciofurnari99@gmail.com>
2023-05-10 20:07:52 -0300

    Add the handle add to cart function with dispatch

--- src/components/wishlist-page/WishlistCard.jsx
+++ src/components/wishlist-page/WishlistCard.jsx
@@ -1,7 +1,18 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { cartDispatchContext } from "../ShopContext";
 
 export default function WishlistCard(props) {
-  const {name, price, img, handlePurchase, handleCart, handleRemove} = props;
+  const {name, price, img, handlePurchase, handleCart, handleRemove,n} = props;
+  const dispatch = useContext(cartDispatchContext)
+
+  function handleAddToCart(ev) {
+    dispatch({
+      type: 'ADD-TO-CART',
+      id: ev.target.id,
+      quantity: 0, // Maybe change the value for 1 // 
+    })
+  }
 
   return (
     <div className="flex flex-col grow items-center justify-center bg-zinc-300">
@@ -11,7 +22,7 @@ export default function WishlistCard(props) {
       <h3 className="text-2xl pt-4 pb-4">{name}</h3>
       <p className="text-xl pb-4">{price}.00 $</p>
       <button className=" pr-6 pl-6 pt-4 pb-4 bg-yellow-800 text-yellow-50 mb-4">Buy it now</button>
-      <button className=" pr-6 pl-6 pt-4 pb-4 bg-yellow-800 text-yellow-50 mb-4">Add to the cart</button>
+      <button id={n} onClick={handleAddToCart}  className=" pr-6 pl-6 pt-4 pb-4 bg-yellow-800 text-yellow-50 mb-4">Add to the cart</button>
       <button className=" pr-6 pl-6 pt-4 pb-4 bg-yellow-800 text-yellow-50 mb-4">Remove</button>
     </div>
   )
