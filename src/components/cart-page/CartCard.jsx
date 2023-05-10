6ed7248bd48c13be7c014cbd0eea5468fa19d594
LucioFurnari <luciofurnari99@gmail.com>
2023-05-09 22:21:08 -0300

    Add the handleRemove item functions, and dispatch

--- src/components/cart-page/CartCard.jsx
+++ src/components/cart-page/CartCard.jsx
@@ -1,12 +1,24 @@
+import { useContext } from "react";
+import { cartDispatchContext } from "../ShopContext";
+
 export default function CartCard(props) {
-  const {name, price, quantity, img, index} = props;
+  const {name, price, quantity, img, id} = props;
+  const dispatch = useContext(cartDispatchContext);
+
+  function handleRemoveItem(ev) {
+    dispatch({
+      type: 'REMOVE-FROM-CART',
+      id: ev.target.id
+    });
+  };
+
   return (
-    <div className=" bg-zinc-300" key={index}>
+    <div className=" bg-zinc-300">
       <img src={img}></img>
       <p className="text-3xl pt-4">{name}</p>
       <p className="text-2xl pt-4">Price: {price} $</p>
       <p className="text-2xl pt-4">{quantity > 1 ? 'Amount: x' + quantity : null}</p>
-      <button className="p-4 pl-8 pr-8 mb-4 text-xl bg-yellow-900 text-yellow-100" id={index} onClick={(ev) => handleDelete(ev, quantity)} >Remove item</button>
+      <button className="p-4 pl-8 pr-8 mb-4 text-xl bg-yellow-900 text-yellow-100" id={id} onClick={(ev) => handleRemoveItem(ev, quantity)} >Remove item</button>
     </div>
   )
 }
