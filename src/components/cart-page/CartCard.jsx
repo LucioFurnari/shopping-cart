159da925edb9fe76873f106bca83ad8f4a2946ed
LucioFurnari <luciofurnari99@gmail.com>
2023-05-10 15:58:12 -0300

    Create the handle function for the change amount and the buttons

--- src/components/cart-page/CartCard.jsx
+++ src/components/cart-page/CartCard.jsx
@@ -12,13 +12,32 @@ export default function CartCard(props) {
     });
   };
 
+  function handleDecreaseAmount(ev) {
+    dispatch({
+      type: 'DECREASE-AMOUNT',
+      id: ev.target.parentNode.id
+    })
+  }
+  function handleIncreaseAmount(ev) {
+    dispatch({
+      type: 'INCREASE-AMOUNT',
+      id: ev.target.parentNode.id
+    })
+  }
+
   return (
     <div className=" bg-zinc-300">
       <img src={img}></img>
       <p className="text-3xl pt-4">{name}</p>
-      <p className="text-2xl pt-4">Price: {price} $</p>
+      <p className="text-2xl pt-4">Price: {price}.00 $</p>
       <p className="text-2xl pt-4">{quantity > 1 ? 'Amount: x' + quantity : null}</p>
-      <button className="p-4 pl-8 pr-8 mb-4 text-xl bg-yellow-900 text-yellow-100" id={id} onClick={(ev) => handleRemoveItem(ev, quantity)} >Remove item</button>
+      <div id={id}>
+        <button className="pl-2 pr-2 bg-zinc-400 text-4xl text-center" onClick={handleDecreaseAmount}>-</button>
+          <span className="p-2 text-2xl">{quantity}</span>
+        <button className="pl-2 pr-2 bg-zinc-400 text-4xl" onClick={handleIncreaseAmount}>+</button>
+      </div>
+      <p className="text-2xl pt-4 font-semibold">Total price: {price * quantity}</p>
+      <button className="p-4 pl-8 pr-8 mb-4 mt-4 text-xl bg-yellow-900 text-yellow-100" id={id} onClick={(ev) => handleRemoveItem(ev, quantity)} >Remove item</button>
     </div>
   )
 }
