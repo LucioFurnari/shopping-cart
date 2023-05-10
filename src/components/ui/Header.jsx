d00f18e2682f2d4e74a04e53e93b96e2f9924b49
LucioFurnari <luciofurnari99@gmail.com>
2023-05-09 22:20:05 -0300

    Minor change in the props of the component

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 
-export default function Header ({ section, link, routeName, item }) {
+export default function Header (props) {
+  const { section, link, routeName, item } = props
   return (
     <div className="flex flex-col  items-center justify-center text-orange-100 bg-zinc-900 pt-20 pb-20">
       <h3 className="text-4xl md:text-6xl mb-6">{section}</h3>
