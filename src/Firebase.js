63e0af9e9f31fce2692dba262f485ab2ce078fd4
LucioFurnari <luciofurnari99@gmail.com>
2023-05-11 19:50:45 -0300

    Create the firebase script

---
+++ src/Firebase.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDpmWBJjZ34fbOl9nWQ1oGJVgj_OffoXdI",
+
+  authDomain: "shopping-cart-94341.firebaseapp.com",
+
+  projectId: "shopping-cart-94341",
+
+  storageBucket: "shopping-cart-94341.appspot.com",
+
+  messagingSenderId: "302435008674",
+
+  appId: "1:302435008674:web:4a1a5dcf9c7561171d17fb"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
