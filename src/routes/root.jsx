57e3621906d65311c0a46fca02186239b552bd45
LucioFurnari <luciofurnari99@gmail.com>
2023-05-11 17:19:14 -0300

    Change the bg of the component to have a arbitrary value

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -11,7 +11,7 @@ export async function loader() {
 export function Root() {
   return(
     <>
-    <div className="home flex flex-col justify-center items-center bg-hero-pattern bg-center bg-cover bg-no-repeat h-screen">
+    <div className="home flex flex-col justify-center items-center bg-[url('./assets/images/background.jpg')] bg-center bg-cover bg-no-repeat h-screen">
       <h2 className="text-4xl md:text-6xl xl:text-8xl text-orange-100">Taste good, does good</h2>
       <Link className="text-4xl  text-orange-100" to='/shop'>To the Shop</Link>
     </div>
