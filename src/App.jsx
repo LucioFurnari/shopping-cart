faec7b3ef59492c5744d43ee681a144bf6ea0b0b
LucioFurnari <luciofurnari99@gmail.com>
2023-05-12 23:18:17 -0300

    Use the data base from firebase firestore for the shop data, and products section

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ import { Loading } from "./components/ui/Loading";
 import { useEffect, useState, Suspense, lazy, createContext, useReducer } from "react";
 const LazyProduct = lazy(() => import("./components/product-page/Product"));
 
+import { db } from "./Firebase";
+import { collection, getDocs, getDoc } from "firebase/firestore";
+
 // Import context //
 import {
   WishListContext,
@@ -21,24 +24,24 @@ import {
 } from "./components/ShopContext";
 
 function App() {
-  // const [cart,setCart] = useState([]);
-  // Wishlist reducer //
+  // Wishlist reducer with wishlist array //
   const [wishlist, dispatch] = useReducer(wishlistReducer, []);
-  // Cart reducer //
+  // Cart reducer with cart array //
   const [cartList, cartDispatch] = useReducer(shopSectionReducer, {cart:[], totalPrice: 0, cartAmount: 0});
+  // Shop database //
+  const [shopData, setData] = useState([])
 
+  useEffect(() => {
+    const getData = async () => {
+      const newData = []
+      const data = await getDocs(collection(db, 'products'));
+      data.forEach((doc) => newData.push(doc.data()))
+      setData(newData)
+    }
+    getData()
+    
+  },[])
 
-  function removeProduct(ev, quantity) {
-    const { id } = ev.target;
-    setCart(cart.filter((item, index) => {
-      if(index != id) {
-        return item
-      } else {
-        setCartTotal(cartTotal - quantity)
-        setTotalPrice(totalPrice - item.price)
-      }
-    }))
-  }
   function completePurchase() {
     setCart([])
     setTotalPrice(0)
@@ -50,28 +53,28 @@ function App() {
     <cartContext.Provider value={ cartList }>
     <cartDispatchContext.Provider value={ cartDispatch }>
     <WishlistDispatchContext.Provider value={dispatch}>
+    <WishListContext.Provider value={wishlist}>
     <Nav />
     <Routes>
       <Route path='/' element={<Root />} />
-      <Route path='/shop' element={<Shop />} />
-        <Route path='/shop/:item' 
+      <Route path='/shop' element={<Shop dataBase={shopData}/>} />
+        <Route path='/shop/:item'
         element={
           <Suspense fallback={<Loading />}>    
-              <LazyProduct />
+              <LazyProduct dataBase={shopData} />
           </Suspense>
-        }
+          }
         />
-      <Route path='/cart' element={
-        <Cart handleDelete={removeProduct} handlePurchase={completePurchase}/>
-      }/>
-      <Route path='/wishlist' element={
-        <WishListContext.Provider value={wishlist}>
-            <Wishlist />
-        </WishListContext.Provider>
-      }></Route>
+      <Route path='/cart'
+      element={
+        <Cart handlePurchase={completePurchase}/>
+        }
+      />
+      <Route path='/wishlist' element={<Wishlist />}></Route>
       <Route path="/*" element={<ErrorPage />}/>
     </Routes>
     <Footer />
+    </WishListContext.Provider>
     </WishlistDispatchContext.Provider>
     </cartDispatchContext.Provider>
     </cartContext.Provider>
