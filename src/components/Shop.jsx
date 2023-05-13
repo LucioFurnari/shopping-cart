76c998cd0d054cee8d58a55503b0e5c179e1cf07
LucioFurnari <luciofurnari99@gmail.com>
2023-05-13 15:56:02 -0300

    Add Header component and change the size of the section

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,19 +1,15 @@
-import dataBase from "./db"
-import { Link } from "react-router-dom"
 import Card from "./Card"
+import Header from "./ui/Header"
 
 export default function Shop(props) {
-  const {func} = props
+  const {dataBase} = props
   return (
-    <div className="shop-section">
-      <div className="flex flex-col  items-center justify-center text-orange-100 bg-zinc-900 pt-20 pb-20">
-        <h2 className="text-4xl md:text-6xl mb-6">Collection</h2>
-        <span className="text-md md:text-xl"><Link to={'/'}>Home</Link> / Products</span>
-      </div>
-      <div className="grid-container p-12 pt-20 gap-6 justify-center items-center grid grid-cols-1 md:grid-col-2  lg:grid-cols-4">
+    <div className="min-h-screen">
+      <Header section='Collection' link='/' routeName='Home' item='Products'/>
+      <div className=" grid-container p-12 pt-20 gap-6 justify-center items-center grid grid-cols-1 md:grid-col-2  lg:grid-cols-4 lg:grid-rows-2">
         {dataBase.map((item,index) => {
-          const { n } = item;
-          return <Card {...item} key={n} id={index} handleClick={func}/>
+          const { id } = item;
+          return <Card {...item} key={id} id={index} />
           })
         }
       </div>
