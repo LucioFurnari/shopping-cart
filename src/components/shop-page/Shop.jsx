ee0dc02396c0381df6b64ef54c83f89cb4d8bedd
LucioFurnari <luciofurnari99@gmail.com>
2023-05-14 01:37:51 -0300

    Add Loading component for the shop section

--- src/components/shop-page/Shop.jsx
+++ src/components/shop-page/Shop.jsx
@@ -1,5 +1,6 @@
 import Card from "./Card"
 import Header from "../ui/Header"
+import { Loading } from "../ui/Loading";
 import { useState, useEffect } from "react";
 import { db } from "../../Firebase";
 import { collection, getDocs, getDoc } from "firebase/firestore";
@@ -10,20 +11,25 @@ export default function Shop(props) {
   // Shop database //
   const [shopData, setData] = useState([])
 
+  // Loading state //
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     const getData = async () => {
       const newData = []
+      setLoading(true)
       const data = await getDocs(collection(db, 'products'));
       data.forEach((doc) => newData.push(doc.data()))
       setData(newData)
+      setLoading(false)
     }
     getData()
-    
   },[])
 
   return (
     <div className="min-h-screen">
       <Header section='Collection' link='/' routeName='Home' item='Products'/>
+      {loading ? <Loading />:
       <div className=" grid-container p-12 pt-20 gap-6 justify-center items-center grid grid-cols-1 md:grid-col-2  lg:grid-cols-4 lg:grid-rows-2">
         {shopData.map((item,index) => {
           const { id } = item;
@@ -31,6 +37,7 @@ export default function Shop(props) {
           })
         }
       </div>
+      }
     </div>
   )
 }
