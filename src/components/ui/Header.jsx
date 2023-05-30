ede882a21fed901c1a92ab9965c79f941efe3aa4
LucioFurnari <luciofurnari99@gmail.com>
2023-05-29 21:37:00 -0300

    Change div for header tag

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 export default function Header (props) {
   const { section, link, routeName, item } = props
   return (
-    <div className="flex flex-col  items-center justify-center text-orange-100 bg-zinc-900 pt-20 pb-20">
+    <header className="flex flex-col  items-center justify-center text-orange-100 bg-zinc-900 pt-20 pb-20">
       <h3 className="text-5xl md:text-6xl mb-6">{section}</h3>
       <span className="text-lg md:text-xl"><Link to={link}>{routeName}</Link> / {item}</span>
-    </div>
+    </header>
   )
 }
