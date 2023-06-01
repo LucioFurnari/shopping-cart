5ce959dd4a835002dd389a448e441b7b006d2af9
LucioFurnari <luciofurnari99@gmail.com>
2023-06-01 15:38:07 -0300

    Create function to handle login and signout of the user

--- src/AuthenticationFunctions.js
+++ src/AuthenticationFunctions.js
@@ -1,6 +1,6 @@
 // Get auth //
 import { auth } from './Firebase'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth"
 
 export async function createUser(email, password) {
   createUserWithEmailAndPassword(auth, email, password )
@@ -14,16 +14,31 @@ export async function createUser(email, password) {
   })
 }
 
-export async function signInUser(email, password) {
-  signInWithEmailAndPassword(auth, email, password)
+export const handleLogIn = (ev, user, password, dispatch) => {
+  ev.preventDefault()
+  // Firebase function to sig In //
+  signInWithEmailAndPassword(auth, user, password)
   .then((userCredential) => {
     // Signed in 
     const user = userCredential.user;
-    console.log(user)
+    // Dispatch to save the user state //
+    dispatch({type: 'LOG-IN', userEmail: user.email})
+    console.log(user.email)
     // ...
   })
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
   });
+}
+
+export const handleSignOut = (dispatch) => {
+  // Use signOut function from Firebase //
+  signOut(auth)
+    .then(() => {
+      // Call the userDispatch to change the state //
+      dispatch({type: 'SIGN-OUT'})
+      console.log('Sign Out succesful')
+    })
+    .catch((error) => console.log('Something happend' + error))
 }
