460929ba708a4b8d4b5eed36af161dc24b6def81
LucioFurnari <luciofurnari99@gmail.com>
2023-06-01 15:29:53 -0300

    Update the component, add handle login function

--- src/components/login-page/loginSection.jsx
+++ src/components/login-page/loginSection.jsx
@@ -1,29 +1,27 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Header from "../ui/Header";
 import { LogInInput } from "../ui/Inputs";
-import { signInUser, data } from "../../AuthenticationFunctions";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../../Firebase";
+
+// Handle Log in function //
+import { handleLogIn } from "../../AuthenticationFunctions";
+// User state dispatch //
+import { userDispatchContext } from "../ShopContext";
 
 export function LogInPage() {
   const [formInputs, setFormInputs] = useState({ "user":'', "user-pass":'' })
-  
+
+  const userDispatch = useContext(userDispatchContext)
 
   const handleInput = (ev) => {
     const { name, value } = ev.target;
     setFormInputs({...formInputs, [name]: value})
   }
 
-  const handleForm = (ev) => {
-    ev.preventDefault()
-    signInUser(formInputs.user, formInputs["user-pass"])
-  }
-
   return (
     <>
     <Header section='Sign-up' link='/' routeName='Home' item='Sign-up'/>
     <main className="flex flex-col items-center">
-      <form onSubmit={handleForm} className="flex flex-col w-[30rem] bg-zinc-100 border-2 my-20">
+      <form onSubmit={(ev) => handleLogIn(ev, formInputs.user, formInputs["user-pass"], userDispatch)} className="flex flex-col w-[30rem] bg-zinc-100 border-2 my-20">
         <LogInInput func={handleInput} name='user' type='text' placeholder='Username'/>
         <LogInInput func={handleInput} name='user-pass' type='text' placeholder='Password' />
         <button type="submit" className="p-4 bg-yellow-800 text-white font-semibold hover:bg-yellow-700 active:bg-yellow-900">Log In</button>
