fee8a0f3120cd74060b3dd002232cdb7d722ab8f
LucioFurnari <luciofurnari99@gmail.com>
2023-06-02 18:24:27 -0300

    Create LogInInput component

--- src/components/ui/Inputs.jsx
+++ src/components/ui/Inputs.jsx
@@ -1,9 +1,16 @@
 export function SignUpInput(props) {
-  const { name, type, placeholder, labelText, id } = props;
+  const { name, type, placeholder, labelText, id, func } = props;
   return(
   <>
   <label className="pl-5 text-xl" htmlFor={id}>{labelText}</label>
-  <input className="px-5 py-2 text-xl focus:outline-black focus:outline focus:outline-1" id={id} type={type} placeholder={placeholder} name={name}></input>
+  <input onChange={func} className="px-5 py-2 text-xl focus:outline-black focus:outline focus:outline-1" id={id} type={type} placeholder={placeholder} name={name}></input>
   </>
+  )
+}
+
+export function LogInInput(props) {
+  const { name, type, placeholder, func } = props;
+  return(
+    <input onChange={func} className="px-5 py-2 text-xl focus:outline-black focus:outline focus:outline-1" name={name} type={type} placeholder={placeholder}></input>
   )
 }
