9de89cff85d98997f4e828dd2da6774f461ebe48
LucioFurnari <luciofurnari99@gmail.com>
2023-06-05 18:50:28 -0300

    Change the contain component to li tag, add pattern and required attribute

--- src/components/ui/Inputs.jsx
+++ src/components/ui/Inputs.jsx
@@ -1,10 +1,12 @@
 export function SignUpInput(props) {
-  const { name, type, placeholder, labelText, id, func } = props;
+  const { name, type, placeholder, labelText, id, func, passValidation, errorMessage, isRequired=false ,patternValue=undefined } = props;
+
   return(
-  <>
-  <label className="pl-5 text-xl" htmlFor={id}>{labelText}</label>
-  <input onChange={func} className="px-5 py-2 text-xl focus:outline-black focus:outline focus:outline-1" id={id} type={type} placeholder={placeholder} name={name}></input>
-  </>
+  <li className="flex flex-col ">
+    <label className="pl-5 text-xl" htmlFor={id}>{labelText}</label>
+    <input required={isRequired} pattern={patternValue} onChange={func} className=" peer px-5 py-2 text-xl focus:outline-gray-400 focus:outline focus:outline-1 invalid:border-b-2 invalid:focus:border-b-red-600" id={id} type={type} placeholder={placeholder} name={name}></input>
+    <span className="invisible peer-invalid:visible ">{errorMessage}</span>
+  </li>
   )
 }
 
