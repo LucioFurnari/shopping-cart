a29a8fdda42df25791a091bfa21f997d28b0c16c
LucioFurnari <luciofurnari99@gmail.com>
2023-06-06 19:06:32 -0300

    Use user state to conditional rendering the user cart and wishlist links

--- src/components/ui/Nav.jsx
+++ src/components/ui/Nav.jsx
@@ -1,22 +1,21 @@
 import { NavLink } from "react-router-dom"
-import CustomLink from "./CustomLink";
+import { CustomLink, UserAuthLinks, UserProfile } from "./CustomLink";
 import { useContext, useEffect, useState } from "react";
 import { AiOutlineShoppingCart, AiOutlineHeart, AiOutlineMenu } from 'react-icons/ai'
-import { BiMenuAltRight } from 'react-icons/bi'
-import { cartContext } from "../ShopContext";
+import { cartContext, userContext } from "../ShopContext";
 
 export default function Nav(props) {
+  // Get cart amount //
   const {cartAmount} = useContext(cartContext);
+  // Get user state //
+  const userState = useContext(userContext)
 
   const [scroll, setScroll] = useState(false);
 
   const [menu, setMenu] = useState(false);
 
-  const [subMenu, setSubMenu] = useState(false);
 
   const handleMenu = () => setMenu(!menu);
-
-  const handleSubMenu = () => setSubMenu(!subMenu);
 
   useEffect(() => {
     const handleScroll = (ev) => {
@@ -43,28 +42,27 @@ export default function Nav(props) {
       <ul className={`${menu && 'right-0'} fixed top-0 -right-full h-screen transition-[right] duration-300 pt-6 px-8 md:p-0 md:h-auto md:static md:flex md:items-center bg-yellow-900 md:bg-transparent`}>
         <CustomLink to='/' linkName='Home'/>
         <CustomLink to='/shop' linkName='Shop' />
-        <CustomLink to='/wishlist' linkName={<AiOutlineHeart className="w-8 h-auto"/>} />
-        <li className="md:ml-4 text-center">
-        <NavLink 
+        {
+        // Use user state to render the links or not //
+        userState.isSigned &&
+          <>
+          <CustomLink to='/wishlist' linkName={<AiOutlineHeart className="w-8 h-auto"/>} />
+          <li className="md:ml-4 text-center">
+          <NavLink 
             to='/cart'
             className={({isActive}) => {
-            return isActive ? "relative text-xl bg-orange-100 text-gray-800 p-2 inline-block rounded-md" 
+            return isActive ? "relative text-xl border-b-2 border-orange-300 text-orange-300 p-2 inline-block" 
             : 
-            'relative transition ease-in-out 150ms text-xl text-orange-100 rounded-md p-2 inline-block hover:bg-orange-100  hover:text-gray-800'
+            'relative transition ease-in-out 150ms text-xl text-gray-200 rounded-md p-2 inline-block   hover:text-orange-300'
             }}
-        >
-            <AiOutlineShoppingCart className="group-hover:fill-gray-800 w-8 h-auto"/> {cartAmount > 0 ? <span className="absolute top-0 right-0">{cartAmount}</span> : null}
-        </NavLink>
-        </li>
-        <li className="md:ml-4 text-center relative">
-          <button className="p-2 hover:bg-orange-100 group rounded-md" onClick={handleSubMenu}>
-            <BiMenuAltRight className="fill-orange-100 group-hover:fill-gray-800 w-8 h-auto"/>
-          </button>
-          <div className={`${!subMenu && 'hidden'} absolute right-full md:right-0 top-0 md:top-full w-40  bg-orange-900 after:content-[''] after:absolute after:-right-[13px] md:after:right-0 after:top-[18px] md:after:-top-[13px] after:-ml-[15px] after:-mt-[15px] after:w-0 after:z-[1] after:h-0 after:border-l-[15px]  after:border-l-orange-900 after:border-t-[15px]  after:border-t-transparent  after:border-b-[15px] after:border-b-transparent md:after:border-b-[15px] md:after:border-b-orange-900 md:after:border-l-[15px] md:after:border-l-transparent md:after:border-r-[15px] md:after:border-r-transparent`}>
-            <NavLink to='/login' className="inline-block w-full text-start p-2">Log In</NavLink>
-            <NavLink to='/register' className="inline-block w-full text-start p-2">Create Account</NavLink>
-          </div>
-        </li>
+          >
+            <AiOutlineShoppingCart className=" w-8 h-auto"/> {cartAmount > 0 ? <span className="absolute top-0 right-0">{cartAmount}</span> : null}
+          </NavLink>
+          </li>
+          </>
+        }
+        <UserAuthLinks />
+        <UserProfile />
       </ul>
       <button className="md:hidden" onClick={handleMenu}>
         {!menu && <AiOutlineMenu aria-label="Menu" className="fill-orange-100 w-12 h-auto"/>}
