d659e36cd86cd2144e597f665b1f243c3e702a9f
LucioFurnari <luciofurnari99@gmail.com>
2023-06-12 22:19:08 -0300

    Move article to a component

---
+++ src/components/ui/Article.jsx
@@ -0,0 +1,21 @@
+import heroImg from '../../assets/images/home-img.jpg'
+
+export default function HeroSection() {
+  return(
+    <article className="hero-section flex justify-center items-center py-8 bg-zinc-900">
+      <section className='px-10 max-w-lg lg:max-w-7xl'>
+        <h2 className='mb-6 text-4xl lg:text-6xl text-zinc-100'>About us</h2>
+        <p className='text-xl text-left text-zinc-100'>
+          Duis lacinia mauris ut sem suscipit cursus. Praesent tincidunt dolor eget pretium porttitor. 
+          Nam malesuada ipsum quis ligula porttitor placerat. 
+          Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 
+          Morbi maximus auctor commodo.
+          Praesent rutrum purus ligula, fringilla dignissim eros pellentesque sit amet. 
+          Integer vel odio rhoncus, dapibus felis ut, venenatis arcu. 
+          Donec dapibus varius ligula et malesuada.
+        </p>
+      </section>
+      <img className='invisible mr-8 rounded-lg w-0 2xl:visible 2xl:w-1/4' src={heroImg}></img>
+    </article>
+  )
+}
