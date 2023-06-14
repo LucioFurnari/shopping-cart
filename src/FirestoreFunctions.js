f39320ae0dbdf787893152f1594a7b76bd5473e3
LucioFurnari <luciofurnari99@gmail.com>
2023-06-14 18:06:22 -0300

    Update, names and add comments to the setItemsCart function

--- src/FirestoreFunctions.js
+++ src/FirestoreFunctions.js
@@ -2,31 +2,35 @@ import { db } from "./Firebase"
 import { doc, getDoc, setDoc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore"
 
 // Set data to the carts collection  WIP //
-export const setItemToCart = async (uid, item, quantity) => {
+export const setItemToCart = async (userId, item, itemQuantity) => {
   const [product] = item
-  const cartRef = doc(db, 'carts', uid)
+  const cartRef = doc(db, 'carts', userId)
   const cartSnap = await getDoc(cartRef)
 
   if (cartSnap.exists()) {
+    // Detect if the array have or not a listed item //
     const hasItem = cartSnap.data().cartItems.some((item) => item.id === product.id)
     if (hasItem) {
+      // Get a copy of the array of cart list //
       const [copyArr] = cartSnap.data().cartItems.filter((item) => item.id === product.id)
-      const quantity = copyArr.quantity
+      const itemQuantity = copyArr.quantity
 
+      // First Remove the exact array and add a new one, firestore need the exact object to be removed and updated //
       await updateDoc(cartRef, {
-        cartItems: arrayRemove({id: product.id, quantity: quantity}),
+        cartItems: arrayRemove({id: product.id, quantity: itemQuantity}),
       })
       await updateDoc(cartRef, {
-        cartItems: arrayUnion({id: product.id, quantity: quantity + 1}),
+        cartItems: arrayUnion({id: product.id, quantity: itemQuantity + 1}),
       })
     } else {
+      // If the item is not in the array, add a new one // 
       await updateDoc(cartRef, {
-        cartItems: arrayUnion({id: product.id, quantity: quantity})
+        cartItems: arrayUnion({id: product.id, quantity: itemQuantity})
       })
     }
   } else {
     await setDoc(cartRef, {
-      cartItems: [{id: product.id, quantity: quantity}],
+      cartItems: [{id: product.id, quantity: itemQuantity}],
     })
   }
 }
