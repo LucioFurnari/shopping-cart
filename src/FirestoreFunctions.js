cc1b94f22ffdc98a77f3acb81b5c79c08b4fbd2f
LucioFurnari <luciofurnari99@gmail.com>
2023-06-17 00:18:16 -0300

    Add a condition, WIP, to increase or decrease the amount

--- src/FirestoreFunctions.js
+++ src/FirestoreFunctions.js
@@ -1,38 +1,54 @@
 import { db } from "./Firebase"
-import { doc, collection, getDoc, getDocs, setDoc, updateDoc, query, where, arrayUnion, arrayRemove } from "firebase/firestore"
+import { doc, collection, getDoc, getDocs, setDoc, updateDoc, query, where, arrayUnion, arrayRemove, onSnapshot } from "firebase/firestore"
 
 // Set data to the carts collection  WIP //
-export const setItemToCart = async (userId, item, itemQuantity) => {
-  const [product] = item
+export const setItemToCart = async (userId, productId, itemQuantity) => {
+  // const [product] = item
   const cartRef = doc(db, 'carts', userId)
   const cartSnap = await getDoc(cartRef)
 
   if (cartSnap.exists()) {
     // Detect if the array have or not a listed item //
-    const hasItem = cartSnap.data().cartItems.some((item) => item.id === product.id)
+    const hasItem = cartSnap.data().cartItems.some((item) => item.id === productId)
     if (hasItem) {
-      // Get a copy of the array of cart list //
-      const [copyArr] = cartSnap.data().cartItems.filter((item) => item.id === product.id)
-      const itemQuantity = copyArr.quantity
+      // Add conditional to increase or decrease the amount //
+      if(itemQuantity > 0) {
+        // Get a copy of the array of cart list //
+        const [copyArr] = cartSnap.data().cartItems.filter((item) => item.id === productId)
+        const itemQuantity = copyArr.quantity
 
-      // First Remove the exact array and add a new one, firestore need the exact object to be removed and updated //
-      await updateDoc(cartRef, {
-        cartItems: arrayRemove({id: product.id, quantity: itemQuantity}),
+        // First Remove the exact array and add a new one, firestore need the exact object to be removed and updated //
+        await updateDoc(cartRef, {
+          cartItems: arrayRemove({id: productId, quantity: itemQuantity}),
+        })
+        await updateDoc(cartRef, {
+          cartItems: arrayUnion({id: productId, quantity: itemQuantity + 1}),
       })
-      await updateDoc(cartRef, {
-        cartItems: arrayUnion({id: product.id, quantity: itemQuantity + 1}),
+      } else {
+        // Get a copy of the array of cart list //
+        const [copyArr] = cartSnap.data().cartItems.filter((item) => item.id === productId)
+        const itemQuantity = copyArr.quantity
+
+        // First Remove the exact array and add a new one, firestore need the exact object to be removed and updated //
+        await updateDoc(cartRef, {
+          cartItems: arrayRemove({id: productId, quantity: itemQuantity}),
+        })
+        await updateDoc(cartRef, {
+        cartItems: arrayUnion({id: productId, quantity: itemQuantity - 1}),
       })
+    }
     } else {
       // If the item is not in the array, add a new one // 
       await updateDoc(cartRef, {
-        cartItems: arrayUnion({id: product.id, quantity: itemQuantity})
+        cartItems: arrayUnion({id: productId, quantity: itemQuantity})
       })
     }
   } else {
     await setDoc(cartRef, {
-      cartItems: [{id: product.id, quantity: itemQuantity}],
+      cartItems: [{id: productId, quantity: itemQuantity}],
     })
   }
+  return true;
 }
 
 // Get data form the carts collection WIP //
@@ -42,7 +58,7 @@ export const getUserCart = async (userId) => {
   const cartSnap = await getDoc(cartRef)
   
   // Create a query
-  const userCartList = []
+  const userCartList = [];
   let totalPrice = 0;
   let totalQuantity = 0;
 
@@ -55,7 +71,6 @@ export const getUserCart = async (userId) => {
 
   const querySnapshots = await Promise.all(requestList)
   querySnapshots.forEach((querySnapshot) => {
-    console.log(querySnapshot)
     querySnapshot.itemData.forEach((doc) => {
       userCartList.push({...doc.data(), quantity: querySnapshot.quantity});
 
@@ -65,4 +80,27 @@ export const getUserCart = async (userId) => {
   });
 
   return ({userCartList, totalPrice, totalQuantity})
+}
+
+export const getUpdateData = async (userId) => {
+  const productsList = collection(db, 'products');
+  const cartRef = doc(db, 'carts', userId);
+  const userCartList = [];
+
+  onSnapshot(cartRef, async (doc) =>  {
+    const requestList = doc.data().cartItems.map( async (item) => {
+      const queryProduct = query(productsList, where('id', '==', item.id))
+      // return object with product data and quantity // 
+      return {itemData: await getDocs(queryProduct), quantity: item.quantity}
+    })
+  
+    const querySnapshots = await Promise.all(requestList)
+    querySnapshots.forEach((querySnapshot) => {
+      querySnapshot.itemData.forEach((doc) => {
+        userCartList.push({...doc.data(), quantity: querySnapshot.quantity});
+      });
+    });
+  })
+
+  return userCartList;
 }
