5ece4f5e895bf811c6b02a59bf38505ea14afc84
LucioFurnari <luciofurnari99@gmail.com>
2023-06-19 20:59:51 -0300

    Add more tags and style

--- src/components/checkout-page/CheckoutPage.jsx
+++ src/components/checkout-page/CheckoutPage.jsx
@@ -1,7 +1,31 @@
+import { useContext } from "react"
+import Header from "../ui/Header"
+import { cartContext } from "../ShopContext"
+
 export function CheckoutPage () {
+  const userCartList = useContext(cartContext)
+  const { cart, cartAmount, totalPrice } = userCartList;
+
   return(
-    <div>
-      
-    </div>
+    <>
+    <Header section='Checkout' link='/cart' routeName='Cart' item='Checkout'/>
+    <main>
+      <div>
+        <h2>Your cart</h2>
+        <ul>
+          {cart.map((product) => {
+            return (
+            <li key={product.id}>
+              <p className='text-red-500 font-semibold text-xl'>{product.name} 
+                <span className='text-black'>{product.price * product.quantity} $</span>
+              </p>
+              <p>x {product.quantity}</p>
+            </li>
+            )
+          })}
+        </ul>
+      </div>
+    </main>
+    </>
   )
 }
