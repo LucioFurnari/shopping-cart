54f323fe49432bb48698f5de636b809327e69c77
LucioFurnari <luciofurnari99@gmail.com>
2023-06-23 19:29:08 -0300

    Add ShippingInformation component

--- src/components/checkout-page/CheckoutPage.jsx
+++ src/components/checkout-page/CheckoutPage.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import Header from "../ui/Header"
 import { cartContext } from "../ShopContext"
+import { ShippingInformation } from "./ShippingInformation"
 
 export function CheckoutPage () {
   const userCartList = useContext(cartContext)
@@ -9,9 +10,10 @@ export function CheckoutPage () {
   return(
     <>
     <Header section='Checkout' link='/cart' routeName='Cart' item='Checkout'/>
-    <main>
-      <div>
-        <h2>Your cart</h2>
+    <main className=" grid grid-cols-2">
+        <ShippingInformation />
+        <section>
+        <h2>Orden summary</h2>
         <ul>
           {cart.map((product) => {
             return (
@@ -24,7 +26,7 @@ export function CheckoutPage () {
             )
           })}
         </ul>
-      </div>
+        </section>
     </main>
     </>
   )
