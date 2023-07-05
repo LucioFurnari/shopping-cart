5649de4c38d99e1b67f4917ce211b3f2cf68e414
LucioFurnari <luciofurnari99@gmail.com>
2023-07-05 15:19:02 -0300

    Change fragment for main tag

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,14 +10,14 @@ export async function loader() {
 
 export function Home() {
   return(
-    <>
+    <main>
     <div className="home flex flex-col justify-center items-center bg-[url('./assets/images/background.jpg')] bg-center bg-cover bg-no-repeat h-screen">
       <h2 className="text-4xl md:text-6xl xl:text-8xl max-w-xl text-center text-white">Taste good, does good</h2>
-      <Link className="text-xl font-semibold mt-10 p-4 rounded-md bg-yellow-800 active:bg-yellow-950 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-slate-100" to='/shop'>To the Shop</Link>
+      <Link className="text-xl font-semibold mt-10 p-4 rounded-md bg-yellow-800 active:bg-yellow-950 hover:bg-yellow-700  text-white focus:outline-none focus:ring-2 focus:ring-slate-100" to='/shop'>To the Shop</Link>
     </div>
       <Article />
       <Gallery />
-    </>
+    </main>
   )
 }
 
