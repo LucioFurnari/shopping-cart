e43ff44335ef6d348b915946e9be9f827d73d1b1
LucioFurnari <luciofurnari99@gmail.com>
2023-07-05 16:37:35 -0300

    Update the style for the sign up form

--- src/components/ui/Inputs.jsx
+++ src/components/ui/Inputs.jsx
@@ -2,10 +2,13 @@ export function SignUpInput(props) {
   const { name, type, placeholder, labelText, id, func, passValidation, errorMessage, isRequired=false ,patternValue=undefined } = props;
 
   return(
-  <li className="flex flex-col ">
-    <label className="pl-5 text-xl" htmlFor={id}>{labelText}</label>
-    <input required={isRequired} pattern={patternValue} onChange={func} className=" peer px-5 py-2 text-xl focus:outline-gray-400 focus:outline focus:outline-1 invalid:border-b-2 invalid:focus:border-b-red-600" id={id} type={type} placeholder={placeholder} name={name}></input>
-    <span className="invisible peer-invalid:visible ">{errorMessage}</span>
+  <li className="flex flex-col mb-2">
+    <fieldset className='relative w-96 peer'>
+      <input required={isRequired} pattern={patternValue} onChange={func} className=' bg-yellow-700/20 outline-none w-full border-b-[1px] border-black p-4 focus:border-b-2 transition-all peer' id={id} type={type} placeholder=' ' name={name} />
+      <label htmlFor={id} className='absolute text-sm left-4 transition-transform peer-focus:-translate-y-0 peer-focus:text-sm peer-placeholder-shown:translate-y-4 peer-placeholder-shown:text-base' >{labelText}</label>
+    </fieldset>
+    <span className="hidden peer-invalid:block">{errorMessage}</span> 
+    {/* <input required={isRequired} pattern={patternValue} onChange={func} className=" peer px-5 py-2 text-xl focus:outline-gray-400 focus:outline focus:outline-1 invalid:border-b-2 invalid:focus:border-b-red-600" id={id} type={type} placeholder={placeholder} name={name}></input> */}
   </li>
   )
 }
