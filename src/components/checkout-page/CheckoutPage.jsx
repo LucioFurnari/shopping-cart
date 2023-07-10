73122f7d00dc8b9fffa67d0d4c71ff2c19580696
LucioFurnari <luciofurnari99@gmail.com>
2023-07-10 15:51:28 -0300

    Create a simple handle to purchase

--- src/components/checkout-page/CheckoutPage.jsx
+++ src/components/checkout-page/CheckoutPage.jsx
@@ -1,26 +1,39 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import Header from "../ui/Header"
 import { cartContext } from "../ShopContext"
 import { ShippingInformation } from "./ShippingInformation"
 
 export function CheckoutPage () {
+  const [activeOverlay, setActiveOverlay] = useState(false);
+
+  const handleOverlay = () => {
+    setActiveOverlay(false)
+  }
+
+  const handlePurchase = () => {
+    setActiveOverlay(true)
+  }
+
   const userCartList = useContext(cartContext)
   const { cart, cartAmount, totalPrice } = userCartList;
 
   return(
     <>
-    <div className='absolute flex items-center justify-center top-0 left-0 w-full h-full  z-[1] bg-gray-500/20'>
-      <div className=' bg-yellow-900 text-white p-6 rounded-xl'>
-        <h2 className=' text-xl mb-3'>Purchase complete</h2>
-        <p className='pr-8 mb-3'>The purchase was completed.</p>
-        <div className='flex justify-end p-3'>
-          <button className=' text-yellow-600'>Continue</button>
+    {
+      activeOverlay &&
+      <div className='absolute flex items-center justify-center top-0 left-0 w-full h-full  z-[1] bg-gray-500/20'>
+        <div className=' bg-yellow-900 text-white p-6 rounded-xl'>
+          <h2 className=' text-xl mb-3'>Purchase complete</h2>
+          <p className='pr-8 mb-3'>The purchase was completed.</p>
+          <div className='flex justify-end p-3'>
+            <button className='text-yellow-600' onClick={handleOverlay}>Continue</button>
+          </div>
         </div>
       </div>
-    </div>
+    }
     <Header section='Checkout' link='/cart' routeName='Cart' item='Checkout'/>
     <main className="grid grid-col-1 md:grid-cols-2 gap-x-8 p-4 px-8 min-h-[calc(100vh-272px)]">
-        <ShippingInformation />
+        <ShippingInformation handlePurchase={handlePurchase} />
         <section className="p-4">
           <h2 className="text-xl font-semibold mb-4">Orden summary</h2>
           <ul className="py-4 row-start-1 md:row-start-auto">
