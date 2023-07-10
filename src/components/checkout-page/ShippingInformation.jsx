4d63f9318d5d5024871923e4c423072bf8e6295c
LucioFurnari <luciofurnari99@gmail.com>
2023-07-10 15:52:07 -0300

    Remove comments, changes tags, add handlePurchase function

--- src/components/checkout-page/ShippingInformation.jsx
+++ src/components/checkout-page/ShippingInformation.jsx
@@ -1,10 +1,17 @@
 
-export function ShippingInformation() {
+export function ShippingInformation(props) {
+  const { handlePurchase } = props
+  const handleForm = (ev) => {
+    ev.preventDefault()
+    handlePurchase()
+  }
+
+
   return (
-    <form className="p-4">
+    <form onSubmit={handleForm} className="p-4 row-start-2 md:row-start-auto">
       <fieldset className="mb-8">
         <legend className="text-xl font-semibold mb-4">Shipping Information</legend>
-        <ul className="grid grid-cols-2">
+        <ul className="grid gap-4 grid-col-1 lg:grid-cols-2">
           <FormInput labelText='First name' inputId='first_name' />
           <FormInput labelText='Last name' inputId='last_name' />
           <FormInput labelText='City' inputId='user_city' />
@@ -13,7 +20,7 @@ export function ShippingInformation() {
           <FormInput labelText='Zip code' inputId='user_zip'/>
         </ul>
       </fieldset>
-      <button className="px-8 py-4 bg-yellow-600 text-white ">Complete purchase</button>
+      <button className="px-8 py-4 bg-yellow-600 hover:bg-yellow-700 text-white ">Complete purchase</button>
     </form>
   )
 }
@@ -21,13 +28,9 @@ export function ShippingInformation() {
 function FormInput(props) {
   const { labelText, inputId, style } = props;
   return (
-    <fieldset className='relative w-96 peer mb-4'>
-      <input className=' bg-yellow-700/20 outline-none w-full border-b-[1px] border-black p-4 focus:border-b-2 transition-all peer' id={inputId} type='text' placeholder=' ' />
+    <fieldset className='relative max-w-[24rem] peer mb-4'>
+      <input className=' bg-yellow-500/20 outline-none w-full border-b-[1px] border-black p-4 focus:border-b-2 transition-all peer' id={inputId} type='text' placeholder=' ' />
       <label htmlFor={inputId} className='absolute text-sm left-4 transition-transform peer-focus:-translate-y-0 peer-focus:text-sm peer-placeholder-shown:translate-y-4 peer-placeholder-shown:text-base' >{labelText}</label>
     </fieldset>
-    // <li className={`${style} py-4 px-2`}>
-    //   <label className="block pb-2" htmlFor={inputId}>{labelText}</label>
-    //   <input className='border-black border-[1px] w-full py-2' type="text" id={inputId}></input>
-    // </li>
   )
 }
